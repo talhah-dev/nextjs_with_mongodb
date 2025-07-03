b468e84160f7f05e56f66bba7cfc3d8e81f4f06b
Muhammad Talha <talha18513@gmail.com>
2025-07-02 19:53:30 -0700

    first commit

--- lib/mongooseConnection.jsx
+++ lib/mongooseConnection.jsx
@@ -1,12 +1,11 @@
-import mongoose from "mongoose";
+// lib/mongooseConnection.js
+import mongoose from 'mongoose'
 
-const mongooseConnection = async () => {
-    try {
-        await mongoose.connect("mongodb+srv://talha18513:v6ElJyjzuHmjCTZV@cluster0.fcrys1b.mongodb.net/")
-        console.log("Connected to MongoDB")
-    } catch (error) {
-        console.log(error)
-    }
+export default async function mongooseConnection() {
+  if (mongoose.connection.readyState >= 1) return
+
+  return mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
 }
-
-export default mongooseConnection;
